3baa2d05728729384e67d8950180ed6c980e7068
myferr <contactme.myfer@protonmail.com>
2025-10-23 16:42:32 -0500

    feat: split main.rs into components

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,8 @@
 use gpui::*;
-use gpui_component::{button::*, *};
+use gpui_component::*;
 
-pub struct HelloWorld;
-impl Render for HelloWorld {
-    fn render(&mut self, _: &mut Window, _: &mut Context<Self>) -> impl IntoElement {
-        div()
-            .v_flex()
-            .gap_2()
-            .size_full()
-            .items_center()
-            .justify_center()
-            .child("Hello, World!")
-            .child(
-                Button::new("ok")
-                    .primary()
-                    .label("Let's Go!")
-                    .on_click(|_, _, _| println!("Clicked!")),
-            )
-    }
-}
+mod components;
+use components::hello_world::HelloWorld;
 
 fn main() {
     let app = Application::new();
